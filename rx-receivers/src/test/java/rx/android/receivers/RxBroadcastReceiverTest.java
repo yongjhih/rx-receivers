42404f25018b8384cfe0b02eadb8f6c2b0a55237
Andrew Chen <yongjhih@gmail.com>
2016-05-25 03:01:42 +0800

    Add subscribeRxAssertions() test

--- rx-receivers/src/test/java/rx/android/receivers/RxBroadcastReceiverTest.java
+++ rx-receivers/src/test/java/rx/android/receivers/RxBroadcastReceiverTest.java
@@ -13,6 +13,8 @@ import rx.observers.TestSubscriber;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.Assert.fail;
+import rx.assertions.RxAssertions;
+import rx.functions.Action0;
 
 @RunWith(RobolectricTestRunner.class) //
 public class RxBroadcastReceiverTest {
@@ -50,5 +52,30 @@ public class RxBroadcastReceiverTest {
     subscription.unsubscribe();
     application.sendBroadcast(intent4);
     o.assertValues(intent1, intent2);
+  }
+
+  @Test public void subscribeRxAssertions() {
+    final Application application = RuntimeEnvironment.application;
+    final Intent intent1 = new Intent("test_action").putExtra("foo", "bar");
+    final Intent intent2 = new Intent("test_action").putExtra("bar", "baz");
+    final Intent intent3 = new Intent("test_action_ignored");
+    final Intent intent4 = new Intent("test_action").putExtra("bar", "baz");
+    RxAssertions.assertThat(RxBroadcastReceiver.create(application, new IntentFilter("test_action"))).emitsNothing().then(new Action0() {
+      @Override public void call() {
+        application.sendBroadcast(intent1);
+      }
+    }).expectedValues(intent1).then(new Action0() {
+      @Override public void call() {
+        application.sendBroadcast(intent2);
+      }
+    }).expectedValues(intent1, intent2).then(new Action0() {
+      @Override public void call() {
+        application.sendBroadcast(intent3);
+      }
+    }).expectedValues(intent1, intent2).unsubscribe().then(new Action0() {
+      @Override public void call() {
+        application.sendBroadcast(intent4);
+      }
+    }).expectedValues(intent1, intent2);
   }
 }
